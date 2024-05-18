58940b399b7ae7f016fa6bf926d9016c4f6716f5
Peter Keffer <peter@keffer.de>
2024-05-18 10:52:13 +0200

    feat: updated h5 visualizer to visualize one big h5 file

--- visualize_h5.py
+++ visualize_h5.py
@@ -7,12 +7,13 @@ import random
 import argparse
 
 
-def visualize_h5_data(h5_file, output_dir=None, plot=False, save_format='jpg'):
+def visualize_h5_data(h5_file, img_id, output_dir=None, plot=False, save_format='jpg'):
     with h5py.File(h5_file, 'r') as f:
-        original_image = f['original_image'][:]
-        retina_warps = f['retina_warps'][:]
-        fixation_history_x = f['fixation_history_x'][:]
-        fixation_history_y = f['fixation_history_y'][:]
+        grp = f[img_id]
+        original_image = grp['original_image'][:]
+        retina_warps = grp['retina_warps'][:]
+        fixation_history_x = grp['fixation_history_x'][:]
+        fixation_history_y = grp['fixation_history_y'][:]
 
         height, width = original_image.shape[:2]
         num_warps = len(retina_warps)
@@ -36,13 +37,14 @@ def visualize_h5_data(h5_file, output_dir=None, plot=False, save_format='jpg'):
             plt.show()
 
         if output_dir:
-            os.makedirs(output_dir, exist_ok=True)
+            img_output_dir = os.path.join(output_dir, img_id)
+            os.makedirs(img_output_dir, exist_ok=True)
 
             # Save the original image with fixations
             original_image_with_fixations = original_image.copy()
             for x, y in zip(fixation_history_x, fixation_history_y):
                 cv2.circle(original_image_with_fixations, (int(x), int(y)), 5, (255, 0, 0), -1)
-            original_image_path = os.path.join(output_dir, f'original_image.{save_format}')
+            original_image_path = os.path.join(img_output_dir, f'original_image.{save_format}')
             if save_format == 'jpg':
                 cv2.imwrite(original_image_path, cv2.cvtColor(original_image_with_fixations, cv2.COLOR_RGB2BGR))
             else:
@@ -55,7 +57,7 @@ def visualize_h5_data(h5_file, output_dir=None, plot=False, save_format='jpg'):
                 warp_coordinates = retina_warps[i, :, :, 3:]  # Take the last 2 dimensions as coordinates
                 for y, x in zip(warp_coordinates[:, :, 0].flatten(), warp_coordinates[:, :, 1].flatten()):
                     cv2.circle(warp_image_with_fixations, (int(x), int(y)), 1, (255, 0, 0), -1)
-                warp_image_path = os.path.join(output_dir, f'retina_warp_{i + 1}.{save_format}')
+                warp_image_path = os.path.join(img_output_dir, f'retina_warp_{i + 1}.{save_format}')
                 if save_format == 'jpg':
                     cv2.imwrite(warp_image_path, cv2.cvtColor(warp_image_with_fixations, cv2.COLOR_RGB2BGR))
                 else:
@@ -64,7 +66,7 @@ def visualize_h5_data(h5_file, output_dir=None, plot=False, save_format='jpg'):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Visualize H5 data with retina warps and fixation history.")
-    parser.add_argument("h5_directory", type=str, help="Directory containing the h5 files.")
+    parser.add_argument("--h5_file", type=str, default="retina_warps/retina_warps.h5", help="Path to the single h5 file containing all retina warps.")
     parser.add_argument("--num_examples", type=int, default=10, help="Number of random examples to visualize.")
     parser.add_argument("--output_dir", type=str, default="visualizations", help="Directory to save the visualizations.")
     parser.add_argument("--plot", action="store_true", help="Whether to plot the visualizations.")
@@ -72,12 +74,11 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    # Get a list of all h5 files in the directory
-    h5_files = [file for file in os.listdir(args.h5_directory) if file.endswith('.h5')]
+    with h5py.File(args.h5_file, 'r') as f:
+        img_ids = list(f.keys())
 
-    # Randomly select num_examples files
-    random_files = random.sample(h5_files, args.num_examples)
+    # Randomly select num_examples image IDs
+    random_img_ids = random.sample(img_ids, args.num_examples)
 
-    for file in random_files:
-        h5_file_path = os.path.join(args.h5_directory, file)
-        visualize_h5_data(h5_file_path, plot=args.plot, output_dir=args.output_dir, save_format=args.save_format)
+    for img_id in random_img_ids:
+        visualize_h5_data(args.h5_file, img_id, plot=args.plot, output_dir=args.output_dir, save_format=args.save_format)
