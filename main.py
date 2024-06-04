bugfix: fixed (un-)normalization

main.py
@@ -314,19 +314,20 @@ class FovealTransform(torch.nn.Module):
 def normalize_fixations(fixations, width, height):
     normalized_fixations = []
     for x, y in fixations:
-        normalized_x = (x - width / 2) / (width / 2)
-        normalized_y = (y - height / 2) / (height / 2)
+        normalized_x = (x - width / 2.0) / (width / 2.0)
+        normalized_y = (y - height / 2.0) / (height / 2.0)
         normalized_fixations.append((normalized_x, normalized_y))
     return normalized_fixations
 
 
 def unnormalize_fixations(fixations, width, height):
-    unnormalized_fixations = []
-    for x, y in fixations:
-        unnormalized_x = (x * (width/2)) + (width/2)
-        unnormalized_y = (y * (height/2)) + (height/2)
-        unnormalized_fixations.append((unnormalized_x, unnormalized_y))
-    return unnormalized_fixations
+  unnormalized_fixations = []
+  for x, y in fixations:
+    # Cast width and height to float for precise division
+    unnormalized_x = (x * (width / 2.0)) + (width / 2.0)
+    unnormalized_y = (y * (height / 2.0)) + (height / 2.0)
+    unnormalized_fixations.append((unnormalized_x, unnormalized_y))
+  return unnormalized_fixations
 
 def get_fixation_history(fixation_coordinates, model):
     """