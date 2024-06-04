7cdd247c55c80a4face5dac4868717f13edb7c00
Peter Keffer <peter@keffer.de>
2024-06-04 15:08:46 +0200

    bugfix: trying to remove the shift

--- main.py
+++ main.py
@@ -171,10 +171,12 @@ class FovealTransform(torch.nn.Module):
         for i in range(0, self.max_x_prime):
             for j in range(0, self.max_y_prime):
                 x, y = self.get_fcg_coordinates(i, j, N_r, roh_0, roh_max, x_0, y_0, inverse=True)
+                print("x", x,y)
+                print( "x0", x_0, y_0)
                 new_coordinates[i, j, 0] = x - x_0  # Subtract x_0 to center the coordinates
                 new_coordinates[i, j, 1] = y_0 - y  # Subtract y from y_0 to invert the y-axis
 
-        new_coordinates = new_coordinates / roh_max  # Normalize the coordinates to the range [-1, 1]
+        #new_coordinates = new_coordinates / roh_max  # Normalize the coordinates to the range [-1, 1]
 
         # create a fovea mask (with ones where the fovea is not), used to add irregularity to peripheral cone locations
         fovea_mask = np.ones((self.max_x_prime, self.max_y_prime, 2))
